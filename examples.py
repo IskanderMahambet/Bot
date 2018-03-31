cf146ebe4d513d10d1a6118d9a9b953445f65a4d
IskanderMahambet <37009713+IskanderMahambet@users.noreply.github.com>
2018-03-31 11:00:57 +0600

    Работа с файлами
    Создали баз продуктов
    Выводим название и количество наименьшего товара

--- examples.py
+++ examples.py
@@ -1,13 +1,12 @@
 file = open('shop.txt','r',encoding='utf-8')
-num=0
+least_product=99999
+leat_line=[]
 for line in file:
     list_values = line.split(',')
-    num=num+1
-    print('Номер: ',num)
-    print('Штрих код: ',list_values[0])
-    print('Название: ',list_values[1])
-    print('Цена: ',list_values[2])
-    print('Срок годности: ',list_values[3])
-    print('Кол-во товара: ',list_values[4])
-
+    if int(list_values[4]) < int(least_product):
+        least_product = int(list_values[4])
+        least_line = list_values
+    
+print('Название товара: ',least_line[1])
+print('Кол-во товара: ',least_line[4])
 
